0901038f69a34f3591bd0bd20a259fb27e433ff0
saeed <saeedjeddimail@gmail.com>
2024-09-16 17:09:19 +0330

    Django backend (#67)
    * Add to column Foodhouse
    
    * Add to column Foodhouse

--- megapp/home/views.py
+++ megapp/home/views.py
@@ -6,4 +6,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class HomeView(LoginRequiredMixin, View):
     template_name = 'interactive_bar.html'
     def get(self, request):
-        return render(request, self.template_name )
+        return render(request, self.template_name)
