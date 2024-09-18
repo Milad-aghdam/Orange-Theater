eeb038a342f7554146e20fa6133209e2e3bba7d7
Milad-aghdam <miladagdam@gmail.com>
2024-09-18 15:13:31 +0330

    fixing open hours bugs

--- megapp/googlebusiness/create_service.py
+++ megapp/googlebusiness/create_service.py
@@ -5,11 +5,19 @@ from googleapiclient.errors import HttpError
 from googleapiclient import discovery
 from googleapiclient.http import build_http
 from oauth2client import client, file, tools
-
+import os 
 
 SCOPES = ['https://www.googleapis.com/auth/business.manage']
-CLIENT_SECRETS_FILE = r'.\docs\client_secrets.json'
-mybusinessaccountmanagement = r'.\docs\mybusinessaccountmanagement.dat'
+CLIENT_SECRETS_FILE = r'.\googlebusiness\docs\client_secrets.json'
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# # Path to client_secrets.json
+# CLIENT_SECRETS_FILE = os.path.join(BASE_DIR, 'docs', 'client_secrets.json')
+
+# print("Client secrets file path:", CLIENT_SECRETS_FILE)  # Log the path to verify
+
+MYBUSINESSMANAGEMENT_FILE = r'.\googlebusiness\docs\mybusinessbusinessinformation.dat'
+# MYBUSINESSMANAGEMENT_FILE = os.path.join(BASE_DIR, 'docs', 'mybusinessbusinessinformation.dat')
 
 def create_service():
     """Fetches business information from Google My Business API."""
@@ -19,10 +27,10 @@ def create_service():
         flow = client.flow_from_clientsecrets(client_secrets, scope=scope, message=tools.message_if_missing(client_secrets))
 
 
-        storage = file.Storage(mybusinessaccountmanagement)
+        storage = file.Storage(MYBUSINESSMANAGEMENT_FILE)
         credentials = storage.get()
         http = credentials.authorize(http=build_http())
-        service = discovery.build("mybusinessaccountmanagement", 'v1', http=http)
+        service = discovery.build("mybusinessbusinessinformation", 'v1', http=http)
         # business_account = service.accounts().list().execute()
         print(service)
         return service
